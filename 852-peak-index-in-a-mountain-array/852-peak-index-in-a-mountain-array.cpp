28149a5cef82576b5ff5b9b50081461ae9be405c
Chinmay Agrawal <chinmayy23@gmail.com>
2022-07-05 16:26:11 +0530

    Time: 23 ms (14.71%), Space: 11.6 MB (48.82%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -4,7 +4,7 @@ public:
         int st=0,en=arr.size()-1;
         while(st<=en){
             int mid=st+(en-st)/2;
-            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+            if(st==en){
                 return mid;
             }
             else if(arr[mid]>arr[mid+1]){
