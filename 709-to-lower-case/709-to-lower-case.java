7c2090553cf414b3aeaeecd7887c957390cda864
Chinmay Agrawal <chinmayy23@gmail.com>
2022-08-14 17:37:57 +0530

    Time: 1 ms (79.59%), Space: 42.6 MB (19.26%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,14 +1,15 @@
 class Solution {
     public String toLowerCase(String s) {
-        StringBuilder sb=new StringBuilder();
+//         StringBuilder sb=new StringBuilder();
     
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
-                sb.append((char)(s.charAt(i)+32));
-            }
-            else{
-                sb.append(s.charAt(i));
-            }
-        }return sb.toString();
+//         for(int i=0;i<s.length();i++){
+//             if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
+//                 sb.append((char)(s.charAt(i)+32));
+//             }
+//             else{
+//                 sb.append(s.charAt(i));
+//             }
+//         }return sb.toString();
+        return s.toLowerCase();
     }
 }
