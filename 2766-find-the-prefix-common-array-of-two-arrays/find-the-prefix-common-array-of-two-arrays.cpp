04ff6af96c5b81f252eb291e64960e630d4de776
Chinmay Agrawal <chinmayy23@gmail.com>
2025-01-14 10:56:35 +0530

    Time: 17 ms (27.65%) | Memory: 88 MB (54.43%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -8,14 +8,22 @@ public:
         for(int i=0;i<n;i++){
             m[A[i]]++;
             m[B[i]]++;
-            temp=0;
-            for(auto it:m){
-                int ind=it.first;
-                int val=it.second;
-                if(val==2){
-                    temp++;
-                }
+            if(m[A[i]]==2){
+                temp++;
+                m.erase(A[i]);
             }
+            if(m[B[i]]==2){
+                temp++;
+                m.erase(B[i]);
+            }
+            // temp=0;
+            // for(auto it:m){
+            //     int ind=it.first;
+            //     int val=it.second;
+            //     if(val==2){
+            //         temp++;
+            //     }
+            // }
             ans.push_back(temp);
         }return ans;
     }
