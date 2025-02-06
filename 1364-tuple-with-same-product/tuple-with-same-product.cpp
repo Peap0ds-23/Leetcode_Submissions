5d51ea997b48717005347dcfd4776c2a0cfa5543
Chinmay Agrawal <chinmayy23@gmail.com>
2025-02-06 09:58:51 +0530

    Time: 889 ms (7.21%) | Memory: 99.3 MB (10.34%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
+        map<int,int> m;
         int count=0;
-        for(int i=0;i<n-3;i++){
-            for(int j=n-1;j>i+2;j--){
-                int st=i+1;
-                int en=j-1;
-                int mid;
-                while(st<en){
-                    mid=st+(en-st)/2;
-                    int sum1=nums[i]*nums[j];
-                    int sum2=nums[en]*nums[st];
-                    if(sum1==sum2){
-                        count+=8;
-                        st++;
-                        en--;
-                    }else if(sum1>sum2){
-                        st++;
-                    }else{
-                        en--;
-                    }
-
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                int pro=nums[i]*nums[j];
+                if(m.find(pro)!=m.end()){
+                    count+=m[pro]*8;
                 }
+                m[pro]++;
             }
         }return count;
     }
