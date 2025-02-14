a34f6e1ebb14acc29bfbd30348e8fab783c3f4e7
Chinmay Agrawal <chinmayy23@gmail.com>
2025-02-14 05:40:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (44.92%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    int money(vector<int>& nums,vector<int>& dp,int n){
-        if(n==0 ){
-            return nums[n];
-        }
-        if(n==1){
-            return max(nums[0],nums[1]);
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int count=0;
-        int left=money(nums,dp,n-1);
-        int right=money(nums,dp,n-2)+nums[n];
-        return dp[n]=max(left,right);
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        if(n==1){
+        vector<int> dp(n+1,0);
+        if(nums.size()==1){
             return nums[0];
         }
-        return money(nums,dp,n-1);
+        if(nums.size()==2){
+            return max(nums[0],nums[1]);
+        }
+        dp[1]=nums[0];
+        dp[0]=0;
+        dp[2]=max(nums[0],nums[1]);
+        for(int i=3;i<=n;i++){
+            dp[i]=max(dp[i-2]+nums[i-1],dp[i-1]);
+        }
+        return dp[n];
     }
-
 };
