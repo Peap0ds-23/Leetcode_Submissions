853480406ef4ee9bdd8ace47f6ce0a028faf98d2
Chinmay Agrawal <chinmayy23@gmail.com>
2025-04-22 20:55:52 +0530

    Time: 15 ms (22.35%) | Memory: 209.4 MB (22.05%) - LeetSync

--- 3738-make-array-non-decreasing/make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int maximumPossibleSize(vector<int>& nums) {
-        vector<int> st;  // will hold the segment maxima in non-decreasing order
+        vector<int> st; 
         for (int x : nums) {
             int currMax = x;
-            // Merge with previous segments while we have an inversion
             while (!st.empty() && st.back() > currMax) {
-                // Merged segment's maximum is the greater of the two:
                 currMax = max(currMax, st.back());
                 st.pop_back();
             }
