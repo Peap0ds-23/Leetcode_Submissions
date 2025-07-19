08df3c9137737822a06267ace04559b261ae20d0
Chinmay Agrawal <chinmayy23@gmail.com>
2025-07-19 18:34:31 +0530

    Time: 3 ms (7.45%) | Memory: 10.8 MB (39.95%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    int fun(vector<int> &dp,vector<int> &nums,int st,int en){
-        dp[st]=nums[st-1];
-        dp[st+1]=max(nums[st],nums[st-1]);
-        for(int i=st+2;i<=en;i++){
-            dp[i]=max(dp[i-2]+nums[i-1],dp[i-1]);
+    int fun(int st,int en,vector<int> &dp,vector<int>& nums){
+        int n=nums.size();
+        if(st==0){
+            dp[0]=nums[0];
+            dp[1]=max(nums[0],nums[1]);
+            for(int i=2;i<n-1;i++){
+                dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+            }
+            return dp[n-2];
+        }else{
+            dp[1]=nums[1];
+            dp[2]=max(nums[1],nums[2]);
+            for(int i=3;i<n;i++){
+                dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+            }
+            return dp[n-1];
         }
-        return dp[en];
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
+        vector<int> dp(n+1,0);
         if(n==1){
             return nums[0];
-        }
-        if(n==2){
+        }else if(n==2){
             return max(nums[0],nums[1]);
         }
-        vector<int> dp(n+1,0);
-        return max(fun(dp,nums,2,n),fun(dp,nums,1,n-1));
+        else if(n==3){
+            return max(max(nums[1],nums[0]),nums[2]);
+        }
+        return max(fun(0,n-2,dp,nums),fun(1,n-1,dp,nums));
     }
 };
