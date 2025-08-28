03bd950ea58d5247100f60045e0ad1a4a8a517a3
Chinmay Agrawal <chinmayy23@gmail.com>
2025-08-28 16:42:45 +0530

    Time: 29 ms (24.17%) | Memory: 37.4 MB (55.76%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -5,20 +5,20 @@ public:
         int n=mat[0].size();
         vector<vector<int>> vis(m,vector<int>(n,0));
         vector<vector<int>> dis(m,vector<int>(n,0));
-        queue<pair<pair<int,int>,int>> q;
+        queue<pair<int,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                    q.push({{i,j},0});
+                    q.push({i,j});
                     dis[i][j]=0;
                     vis[i][j]=1;
                 }
             }
         }
         while(!q.empty()){
-            int nodei=q.front().first.first;
-            int nodej=q.front().first.second;
-            int dist=q.front().second;
+            int nodei=q.front().first;
+            int nodej=q.front().second;
+            // int dist=q.front().second;
             q.pop();
             int row[]={-1,0,1,0};
             int col[]={0,1,0,-1};
@@ -27,8 +27,8 @@ public:
                 int nc=nodej+col[i];
                 if(nr>=0 && nr<m && nc>=0 && nc<n && mat[nr][nc]==1 && !vis[nr][nc]){
                     vis[nr][nc]=1;
-                    q.push({{nr,nc},dist+1});
-                    dis[nr][nc]=dist+1;
+                    q.push({nr,nc});
+                    dis[nr][nc]=dis[nodei][nodej]+1;
                 }
             }
         }return dis;
