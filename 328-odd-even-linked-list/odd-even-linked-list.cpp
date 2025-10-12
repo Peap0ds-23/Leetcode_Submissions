626b8b8cd1f88ffe6ef5af8d57f1153b881a2fdb
Chinmay Agrawal <chinmayy23@gmail.com>
2025-10-12 13:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (28.94%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -14,26 +14,52 @@ public:
         if(!head || !head->next || !head->next->next){
             return head;
         }
-        ListNode* odd=head;
-        ListNode* odd2=head;
-        ListNode* even=head->next;
-        ListNode* even2=head->next;
-        while(odd && even){
-            if(even){
-                odd->next=even->next;
+        ListNode* curr=head;
+        ListNode* odd=new ListNode(1);
+        ListNode* odd2=odd;
+        ListNode* even=new ListNode(2);
+        ListNode* even2=even;
+        while(curr){
+            if(curr){
+                odd->next=curr;
+                curr=curr->next;
                 odd=odd->next;
             }
-            
-            if(odd){
-                even->next=odd->next;
+            if(curr){
+                even->next=curr;
+                curr=curr->next;
                 even=even->next;
-            } 
+            }
         }
-        odd=odd2;
-        while(odd->next){
-            odd=odd->next;
+        even->next=NULL;
+        if(odd && even2){
+            odd->next=even2->next;
         }
-        odd->next=even2;
-        return odd2;
+        if(odd2){
+            return odd2->next;
+        }
+        return NULL;
+        
+        // ListNode* odd=head;
+        // ListNode* odd2=head;
+        // ListNode* even=head->next;
+        // ListNode* even2=head->next;
+        // while(odd && even){
+        //     if(even){
+        //         odd->next=even->next;
+        //         odd=odd->next;
+        //     }
+            
+        //     if(odd){
+        //         even->next=odd->next;
+        //         even=even->next;
+        //     } 
+        // }
+        // odd=odd2;
+        // while(odd->next){
+        //     odd=odd->next;
+        // }
+        // odd->next=even2;
+        // return odd2;
     }
 };
