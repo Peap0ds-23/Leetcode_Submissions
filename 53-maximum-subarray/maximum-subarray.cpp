e6857200677b88c4ec7275f2773f72cdd9008c98
Chinmay Agrawal <chinmayy23@gmail.com>
2025-10-19 14:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (80.37%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,12 +3,26 @@ public:
     int maxSubArray(vector<int>& nums) {
         int sum=0;
         int maxx=INT_MIN;
+        int st=0,en=0,curr;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
             maxx=max(maxx,sum);
             if(sum<0){
                 sum=0;
             }
+            // if(sum==0){
+            //     curr=i;
+            // }
+            // sum+=nums[i];
+            // if(sum>=maxx){
+            //     en=i;
+            //     maxx=sum;
+            // }
+            // // maxx=max(maxx,sum);
+            // if(sum<0){
+            //     sum=0;
+            //     st=curr;
+            // }
         }return maxx;
     }
 };
