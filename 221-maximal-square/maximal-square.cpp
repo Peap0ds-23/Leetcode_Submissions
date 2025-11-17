a343648940a4d7b74eaf8be46fe7ccd55022a55f
Chinmay Agrawal <chinmayy23@gmail.com>
2025-11-17 19:40:18 +0530

    Time: 7 ms (73.26%) | Memory: 30.9 MB (78.83%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,90 +1,26 @@
 class Solution {
 public:
-    vector<int> NSL(vector<int> &temp){
-        vector<int> nsl;
-        stack<int> st;
-        for(int i=0;i<temp.size();i++){
-            if(st.empty()){
-                nsl.push_back(-1);
-            }else if(temp[st.top()]<temp[i]){
-                nsl.push_back(st.top());
-            }else{
-                while(!st.empty() && temp[st.top()]>=temp[i]){
-                    st.pop();
-                }
-                if(st.empty()){
-                    nsl.push_back(-1);
-                }else if(temp[st.top()]<temp[i]){
-                    nsl.push_back(st.top());
-                }
-            }
-            st.push(i);
-        }
-        return nsl;
-    }
-    vector<int> NSR(vector<int> &temp){
-        vector<int> nsr;
-        stack<int> st;
-        int n=temp.size();
-        for(int i=n-1;i>=0;i--){
-            if(st.empty()){
-                nsr.push_back(n);
-            }else if(temp[st.top()]<temp[i]){
-                nsr.push_back(st.top());
-            }else{
-                while(!st.empty() && temp[st.top()]>=temp[i]){
-                    st.pop();
-                }
-                if(st.empty()){
-                    nsr.push_back(n);
-                }else if(temp[st.top()]<temp[i]){
-                    nsr.push_back(st.top());
-                }
-            }
-            st.push(i);
-        }
-        reverse(nsr.begin(),nsr.end());
-        return nsr;
-    }
-    int maxAreaOfHistogram(vector<int> &temp){
-        vector<int> nsl=NSL(temp);
-        vector<int> nsr=NSR(temp);
-        int n=temp.size();
-        int maxx=0;
-        for(int i=0;i<n;i++){
-            // cout<<"nsl:"<<nsl[i]<<" "<<"nsr:"<<nsr[i]<<endl;
-            int hori=(nsr[i]-nsl[i]-1);
-            int vert=temp[i];
-            int ind=min(hori,vert);
-            ind*=ind;
-            // ind*=temp[i];
-            maxx=max(maxx,ind);
-        }
-        // cout<<"--------"<<endl;
-        return maxx;
-
-    }
     int maximalSquare(vector<vector<char>>& matrix) {
         int m=matrix.size(),n=matrix[0].size();
         vector<vector<int>> dp(m,vector<int>(n,0));
-        for(int j=0;j<n;j++){
-            dp[0][j]=matrix[0][j]-'0';
-        }
-        for(int i=1;i<m;i++){
+        int ans=0;
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]=='1'){
-                    dp[i][j]=dp[i-1][j]+1;
+                    int up=(i==0 || matrix[i-1][j]=='0')?0:dp[i-1][j];
+                    int left=(j==0 || matrix[i][j-1]=='0')?0:dp[i][j-1];
+                    int dia=((i==0 || j==0) || matrix[i-1][j-1]=='0')?0:dp[i-1][j-1];
+
+                    dp[i][j]=1+min(up,min(left,dia));
                 }else{
                     dp[i][j]=0;
                 }
+                ans=max(ans,dp[i][j]);
+                // cout<<dp[i][j]<<" ";
+
             }
+            // cout<<endl;
         }
-        int ans=0;
-        for(int i=0;i<m;i++){
-            vector<int> &temp=dp[i];
-            int area=maxAreaOfHistogram(temp);
-            ans=max(ans,area);
-        }
-        return ans;
+        return ans*ans;
     }
 };
