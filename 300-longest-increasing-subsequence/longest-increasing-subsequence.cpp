ad8ef7665cb6a4f9ba0a388197b4855d6b0094d6
Chinmay Agrawal <chinmayy23@gmail.com>
2025-11-17 23:19:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (99.26%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n,1);
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i] && dp[j]+1>dp[i]){
-                    dp[i]=1+dp[j];
-                }
+    int BS(vector<int> &temp,int target){
+        int st=0,en=temp.size()-1;
+        int ans=-1;
+        while(st<=en){
+            int mid=st+(en-st)/2;
+            if(temp[mid]==target){
+                return mid;
+            }else if(temp[mid]>target){
+                ans=mid;
+                en=mid-1;
+            }else{
+                st=mid+1;
             }
         }
+        return ans;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> temp;
+        temp.push_back(nums[0]);
         int len=1;
-        for(int i=0;i<n;i++){
-            len=max(len,dp[i]);
+        for(int i=1;i<n;i++){
+            if(nums[i]>temp.back()){
+                len++;
+                temp.push_back(nums[i]);
+            }else{
+                int lb=BS(temp,nums[i]);
+                temp[lb]=nums[i];
+            }
         }
         return len;
     }
