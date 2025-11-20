7f9b75027782ae20be765ea9e80a6cba7bc4371c
Chinmay Agrawal <chinmayy23@gmail.com>
2025-11-20 13:41:33 +0530

    Time: 3 ms (71.28%) | Memory: 11.1 MB (53.40%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        stack<char> st;
+        for(auto it:expression){
+            if(it=='(' || it==','){
+                continue;
+            }else if(it=='t' || it=='f' || it=='&' || it=='|' || it=='!'){
+                st.push(it);
+            }else if(it==')'){
+                bool hasTrue=false,hasFalse=false;
+                while(!st.empty() && st.top()!='&' && st.top()!='|' && st.top()!='!'){
+                    if(st.top()=='t'){
+                        hasTrue=true;
+                    }
+                    if(st.top()=='f'){
+                        hasFalse=true;
+                    }
+                    st.pop();
+                }
+                char ch=st.top();
+                st.pop();
+                if(ch=='!'){
+                    st.push((hasTrue)?'f':'t');
+                }else if(ch=='|'){
+                    st.push((hasTrue)?'t':'f');
+                }else{
+                    st.push((hasFalse)?'f':'t');
+                }
+            }
+        }
+        return st.top()=='t';
+    }
+};
