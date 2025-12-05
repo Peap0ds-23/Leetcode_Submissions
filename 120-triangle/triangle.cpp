131481296600f7823e2d0ccb74ddd4d3067cf486
Chinmay Agrawal <chinmayy23@gmail.com>
2025-12-05 18:20:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (17.87%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int fun(vector<vector<int>>& triangle,vector<vector<int>>& dp,int i,int j,int m,int n){
-        if(i<0 || i>=n || j<0 || j>i){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int left=fun(triangle,dp,i+1,j,m,n)+triangle[i][j];
-        int right=fun(triangle,dp,i+1,j+1,m,n)+triangle[i][j];
-        return dp[i][j]=min(left,right);
+        if (i == m - 1) return dp[i][j] = triangle[i][j];
+        if (dp[i][j] != INT_MAX) return dp[i][j];
+
+        int left  = fun(triangle, dp, i + 1, j,     m,n);
+        int right = fun(triangle, dp, i + 1, j + 1, m,n);
+
+        return dp[i][j] = triangle[i][j] + min(left, right);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
         return fun(triangle,dp,0,0,n,n);
     }
 };
