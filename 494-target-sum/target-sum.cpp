fd57154e643d1f2a4c38e8dca67e5d62b4ac55ae
Chinmay Agrawal <chinmayy23@gmail.com>
2025-12-05 20:32:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (83.16%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,50 +1,52 @@
 class Solution {
 public:
-     //This question is same as number of subsets with given difference 
-    int countSubsets(vector<int>& nums, int n, int M)
-    {
-        int t[n+1][M+1];
+    // This question is same as number of subsets with given difference
+    int countSubsets(vector<int>& nums, int n, int M) {
+        int t[n + 1][M + 1];
+        int k = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= M; j++) {
+                if (i == 0)
+                    t[i][j] = 0;
+                if (j == 0) {
+                    if (i == 0 || nums[i - 1] == 0) {
+                        t[i][j] = pow(2, k);
+                        k++;
+                    } else {
+                        t[i][j] = t[i - 1][j];
+                    }
 
-        for(int i=0; i<=n; i++)
-        {
-            for(int j=0; j<=M; j++)
-            {
-                if(i==0)
-                    t[i][j]=0;
-                if(j==0)
-                    t[i][j]=1;
+                    // k++;
+                }
             }
         }
 
         // t[0][0] = 1;
 
-        for(int i=1; i<=n; i++)
-        {
-            for(int j=0; j<=M; j++)
-            {
-                if(nums[i-1]<=j)
-                    t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (nums[i - 1] <= j)
+                    t[i][j] = t[i - 1][j - nums[i - 1]] + t[i - 1][j];
                 else
-                    t[i][j]=t[i-1][j];
+                    t[i][j] = t[i - 1][j];
             }
         }
 
-        return t[n][M];  
+        return t[n][M];
     }
 
-    int findTargetSumWays(vector<int>& nums, int target)
-    {
-         target=abs(target);
-         int n=nums.size();
-         int sum=0;
+    int findTargetSumWays(vector<int>& nums, int target) {
+        target = abs(target);
+        int n = nums.size();
+        int sum = 0;
 
-         for(int i=0; i<n; i++)
-             sum+=nums[i];
+        for (int i = 0; i < n; i++)
+            sum += nums[i];
 
-        int M=(sum+target)/2;
-        if(sum<target||(sum+target)%2!=0)
+        int M = (sum + target) / 2;
+        if (sum < target || (sum + target) % 2 != 0)
             return 0;
-        
-         return countSubsets(nums, n, M);
-    }  
+
+        return countSubsets(nums, n, M);
+    }
 };
