63bdcf2005e077ac0004a0f10f022005f6afe952
Chinmay Agrawal <chinmayy23@gmail.com>
2026-01-13 13:06:29 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (82.69%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,31 +11,24 @@
  */
 class Solution {
 public:
-    bool isBalanced(TreeNode* root) {
-        int ans=fun(root);
-        if(ans==-1){
-            return false;
-        }return true;
-    }
-
     int fun(TreeNode* root){
-
         if(!root){
             return 0;
         }
-        // if(!(root->left) && !(root->right)){
-        //     return 1;
-        // }
         int left=fun(root->left);
         int right=fun(root->right);
-        if(left==-1 || right ==-1){
+        if(left==-1 || right==-1){
             return -1;
         }
-        if(abs(left-right)<=1){
-            return 1+max(left,right);
-        }else{
+        if(abs(left-right)>1){
             return -1;
         }
-        
+        return 1+max(left,right);
+    }
+    bool isBalanced(TreeNode* root) {
+        if(fun(root)==-1){
+            return false;
+        }
+        return true;
     }
 };
