e66442c5bb9043fc6a9174aaeb3004f9a9c73048
Chinmay Agrawal <chinmayy23@gmail.com>
2026-01-16 17:11:41 +0530

    Time: 4 ms (7.26%) | Memory: 33.2 MB (6.45%) - LeetSync

---
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> s;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            s.insert(nums[i]);
+        }
+        int maxx=0;
+        for(int i=0;i<n;i++){
+            int count=1;
+            int j=1;
+            while(s.find(nums[i]-j)!=s.end()){
+                s.erase(nums[i]-j);
+                count++;
+                j++;
+            }
+            j=1;
+            while(s.find(nums[i]+j)!=s.end()){
+                s.erase(nums[i]+j);
+                count++;
+                j++;
+            }
+            maxx=max(maxx,count);
+        }return maxx;
+    }
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        int a=longestConsecutive(hBars);
+        int b=longestConsecutive(vBars);
+        int minn=min(a,b)+1;
+        return minn*minn;
+    }
+};
